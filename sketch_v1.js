4a654f683e4b7db4d151532e4bcd529ebbff9cea
Udi Lumnitz <u.lumnitz@applicaster.com>
2019-12-27 12:08:53 +0200

    adding v1

---
+++ sketch_v1.js
@@ -0,0 +1,47 @@
+var table;
+let fr = 10;
+var rows;
+var sortedRows;
+var sortedRowsCount;
+
+function preload() {
+  table = loadTable(
+    "climate-change-earth-surface-temperature-data/GlobalTemperatures.csv",
+    "csv",
+    "header"
+  );
+  frameRate(fr);
+}
+
+function setup() {
+  createCanvas(800, 800);
+  rows = table.getRows();
+  sortedRows = rows.sort((a, b) => b.dt - a.dt);
+  sortedRowsCount = sortedRows.length;
+  loop();
+}
+
+function draw() {
+  background(0);
+  stroke(0);
+
+  for (var i = 0; i < sortedRowsCount; i++) {
+    var avgTemp = float(sortedRows[i].getString("LandAverageTemperature"));
+    var date = sortedRows[i].getString("dt");
+    var randomY = random(800);
+    fill(tempColor(avgTemp));
+    rect(i, randomY, avgTemp * 10, avgTemp * 50);
+  }
+}
+
+function tempColor(temp) {
+  if (temp <= 5) {
+    return "#80FF00";
+  } else if (temp >= 6 && temp <= 11) {
+    return "#FF8000";
+  } else if (temp >= 12) {
+    return "#CC0000";
+  } else {
+    return 0;
+  }
+}
---
+++ sketch_v2.js
@@ -0,0 +1,58 @@
+var table;
+let fr = 20;
+var data;
+
+function preload() {
+  table = loadTable(
+    "climate-change-earth-surface-temperature-data/GlobalLandTemperaturesByMajorCity.csv",
+    "csv",
+    "header"
+  );
+  frameRate(fr);
+}
+
+function setup() {
+  createCanvas(800,800);
+  data = tableCleanup(table);
+  print(data);
+  loop();
+}
+
+function draw() {
+  background(0);
+  stroke(0);
+
+  for (var i = 0; i < data.length; i++) {
+
+    var avgTemp = float(data[i].getString("AverageTemperature"));
+    //var date = data[i].getString("dt");
+    //var city = data[i].getString("City");
+    var long = float(data[i].getString("Longitude"));
+    var lat = float(data[i].getString("Latitude"));
+    
+    var x = map(long,-180,180,0,width);
+    var y = map(lat,-90,90,height,0);
+
+    fill(tempColor(avgTemp));
+    rect(x, y, avgTemp/2, avgTemp*2);
+  }
+}
+
+function tempColor(temp) {
+  if (temp <= 8) {
+    return "#80FF00";
+  } else if (temp >= 9 && temp <= 16) {
+    return "#FF8000";
+  } else if (temp >= 17) {
+    return "#CC0000";
+  } else {
+    return 0;
+  }
+}
+
+function tableCleanup (tableA) {
+    arr = tableA.getRows();
+    
+
+    return arr;
+}
